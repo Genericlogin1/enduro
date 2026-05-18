ff703a27d986266281f4240f96980cdbba8b25e4
Genericlogin1 <Sergeychestno@gmail.com>
2026-05-18 16:55:20 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: '**.supabase.co' },
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
